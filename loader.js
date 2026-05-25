74877f35baa67afdd3a92fb862d121cc308db3bb
mumbojumbo <mittensthecats123@gmail.com>
2026-05-25 18:51:27 -0400

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,2 +1,11 @@
-// Redirect to the main app HTML (adjust filename if yours is different)
-window.location.href = 'app.html';
+// Fetch the full app HTML and replace this page with it
+fetch('app.html')
+  .then(response => response.text())
+  .then(html => {
+    document.open();
+    document.write(html);
+    document.close();
+  })
+  .catch(err => {
+    document.body.innerHTML = '<p style="color:red">Failed to load app.html – make sure it is in the same folder.</p>';
+  });
